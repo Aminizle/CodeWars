83c2f27f7c4365e262236b38686256f47ff84f6a
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2022-05-20 23:49:04 -0400

    Format changes

--- 8kyu/8-sentence-smash.js
+++ 8kyu/8-sentence-smash.js
@@ -21,11 +21,6 @@ EXAMPLE
 
 PSEUDO CODE
 1. Create a function that takes in an array of Strings and returns a concatinated String with spaces in between but not before first String or after last String.
-
-NOTE
-Array.prototype.join()
-The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
 */
 
 
@@ -41,3 +36,9 @@ console.log(wordArray);
 var wordArray1 = smash(['hello', 'world']);
 console.log(wordArray1);
 
+/*
+NOTE
+Array.prototype.join()
+The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+*/
