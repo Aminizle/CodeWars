ea5dedd37a708241538d509af78f9b01bcce7b96
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-01-04 22:15:06 -0500

    added counting sheep kata

---
+++ 8kyu/8-Counting-Sheep.js
@@ -0,0 +1,68 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////////////////////////////////////////////
+// Counting sheep...
+//////////////////////////////////////////////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+/* 
+Kata URL
+https://www.codewars.com/kata/54edbc7200b811e956000556
+
+
+Description:
+
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+Arrays
+Fundamentals
+
+PSEUDO CODE
+1. Create a function that iterates through the array.
+2. each iteration should validate if "true" is present.
+3. if "true" is found it should add +1 to the sheeps present sum number.
+*/
+
+
+// SOLUTION
+function countSheeps(arrayOfSheep) {
+    let counter = 0;
+    for(let i = 0; i < arrayOfSheep.length; i++) {
+        if(arrayOfSheep[i]){
+          counter += 1;
+        } 
+    }
+    return counter
+}
+
+// TEST CASES
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+var array1 = [true,  true,  true,  false,
+              true,  true,  true,  true ,
+              true,  false, true,  false,
+              true,  false, false, true ,
+              true,  true,  true,  true ,
+              false, false, true,  true ];
+              
+Test.assertEquals(countSheeps(array1), 17, "There are 17 sheeps in total")
+  });
+});
+
+/*
+NOTE
+
+*/
