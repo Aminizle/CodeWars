e859a6f5e47e2ebe0e872693df64d746f8e4660f
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-05-25 18:57:56 -0400

    s

--- Interview Questions/AddZeroToEnd.js
+++ Interview Questions/AddZeroToEnd.js
@@ -14,3 +14,25 @@ function zeroAdjuster(numbers) {
 //example [0,1,2,3] //  should return 1,2,3,0
 
 //shared via https://sharepad.io/live/QlHAHuI
+
+// Solution:
+// function moveZerosToEnd(arr) {
+//     let zeros = [];
+//     let nonZeros = [];
+    
+//     // Separate zeros and non-zeros into different arrays
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] === 0) {
+//         zeros.push(arr[i]);
+//       } else {
+//         nonZeros.push(arr[i]);
+//       }
+//     }
+    
+//     // Concatenate the two arrays
+//     return nonZeros.concat(zeros);
+//   }
+  
+// let arr = [1, 0, 2, 0, 3, 4];
+// let newArr = moveZerosToEnd(arr);
+// console.log(newArr); // [1, 2, 3, 4, 0, 0]
